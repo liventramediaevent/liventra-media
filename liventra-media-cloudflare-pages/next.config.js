792ead7ac1c720e7a904188a553c0e8f7bb723be
liventramediaevent <liventramedia@gmail.com>
2026-06-23 22:09:13 +0530

    Update next.config.js

--- liventra-media-cloudflare-pages/next.config.js
+++ liventra-media-cloudflare-pages/next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "i.ytimg.com" },
-      { protocol: "https", hostname: "yt3.ggpht.com" }
-    ]
-  }
-};
-module.exports = nextConfig;
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
