1c06597cc0acd41a9f37a68fd1bf4dc6569f687d
liventramediaevent <liventramedia@gmail.com>
2026-06-23 22:56:29 +0530

    Update page.tsx

--- liventra-media-cloudflare-pages/app/page.tsx
+++ liventra-media-cloudflare-pages/app/page.tsx
@@ -10,16 +10,40 @@ export const revalidate = 60;
 
 export default async function HomePage() {
   const data = await getYouTubeData();
+
   return (
     <main>
       <Header />
-      <Hero isLive={data.isLive} channelTitle={data.channelTitle} liveTitle={data.liveTitle} />
-      <LivePlayer isLive={data.isLive} liveVideoId={data.liveVideoId} />
-      <div className="container-main py-2"><div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" /></div>
-      <LiveChat isLive={data.isLive} liveVideoId={data.liveVideoId} />
-      <div className="container-main py-2"><div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" /></div>
+      <Hero
+        isLive={data.isLive}
+        channelTitle={data.channelTitle}
+        liveTitle={data.liveTitle}
+      />
+
+      <LivePlayer
+        isLive={data.isLive}
+        liveVideoId={data.displayVideoId}
+      />
+
+      <div className="container-main py-2">
+        <div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" />
+      </div>
+
+      <LiveChat
+        isLive={data.isLive}
+        liveVideoId={data.liveVideoId}
+      />
+
+      <div className="container-main py-2">
+        <div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" />
+      </div>
+
       <RecentVideos videos={data.recentVideos} />
-      <div className="container-main py-2"><div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" /></div>
+
+      <div className="container-main py-2">
+        <div className="h-px bg-gradient-to-r from-transparent via-white/10 to-transparent" />
+      </div>
+
       <AboutSection />
     </main>
   );
